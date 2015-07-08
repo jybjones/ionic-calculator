d58dc14a2ec0172f55033c28d669f11aa11c9792
Jennifer Jones <jybjones@gmail.com>
2015-07-08 16:34:25 -0500

    have multiple integers in add and subtract

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -6,33 +6,46 @@ angular.module('starter')
   $scope.result = 0;
   $scope.operation = null;
 
-    $scope.x = 0;
-    $scope.y = 0;
-
+    $scope.x = "0";
+    $scope.y = "0";
+//////////2 numbers////////
   $scope.number = function (num) {
-    $scope.display=num;
     if($scope.operation ===null){
-      $scope.x = num;
+      $scope.display+=num.toString();
+      $scope.x += num;
     }else{
-      $scope.y=num;
+      $scope.display+=num.toString();
+      $scope.y += num;
     }
   };
 
   $scope.add = function (num1, num2) {
-
     $scope.operation ='+';
     $scope.result = num1 + num2
+    $scope.display = '';
+    };
+  $scope.subtract = function (num1, num2) {
+    $scope.operation ='-';
+    $scope.result = num1 - num2
+    $scope.display = '';
     };
 
+    $scope.tan=function() {
+
+  };
+
+////////////////calculations here///////////
 $scope.onClickCalculate = function() {
+  $scope.x = parseInt($scope.x);
+   $scope.y = parseInt($scope.y);
   if ($scope.operation === '+') {
     $scope.add($scope.x,$scope.y);
     $scope.display = $scope.result;
+    }else if ($scope.operation === '-'){
+      $scope.subtract($scope.x,$scope.y);
+      $scope.display = $scope.result;
   }
 }
 
- $scope.tan=function() {
-
-  }
 
 }]);
