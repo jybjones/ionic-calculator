9f3d7466961fe4b592d608d6b75f78965587c680
Jennifer Jones <jybjones@gmail.com>
2015-07-09 09:41:05 -0500

    simple arithmetic functions work

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -5,10 +5,10 @@ angular.module('starter')
   $scope.display = 0;
   $scope.result = 0;
   $scope.operation = null;
-
+  $scope.clearAll = 0;
     $scope.x = "0";
     $scope.y = "0";
-//////////2 numbers////////
+
   $scope.number = function (num) {
     if($scope.operation ===null){
       $scope.display+=num.toString();
@@ -18,6 +18,13 @@ angular.module('starter')
       $scope.y += num;
     }
   };
+  $scope.clearAll = function (num1, num2) {
+    $scope.operation =null;
+    $scope.x = "0";
+    $scope.y = "0";
+    $scope.result = "0";
+    $scope.display = "";
+    };
 
   $scope.add = function (num1, num2) {
     $scope.operation ='+';
@@ -29,22 +36,61 @@ angular.module('starter')
     $scope.result = num1 - num2
     $scope.display = '';
     };
+  $scope.multiply = function (num1, num2) {
+    $scope.operation ='*';
+    $scope.result = num1 * num2
+    $scope.display = '';
+    };
+  $scope.divide = function (num1, num2) {
+    $scope.operation ='&#47';
+    $scope.result = num1 / num2
+    $scope.display = '';
+    };
+   $scope.sin = function (num) {
+    $scope.operation = '&#x207b;&sup1';
+    $scope.result = Math.sin(num)
+    $scope.display = '';
+    };
+  // $scope.squared = function (num1) {
+  //   $scope.operation = &radic;
+  //   $scope.result = num1 * num1
+  //   $scope.display = '';
+  //   };
 
-    $scope.tan=function() {
-
-  };
-
-////////////////calculations here///////////
+////////////////actions here///////////
 $scope.onClickCalculate = function() {
   $scope.x = parseInt($scope.x);
    $scope.y = parseInt($scope.y);
+
+   if ($scope.operation === 'clearAll'){
+    $scope.clearAll($scope.x, $scope.y);
+    $scope.display = $scope.result;
+   }
+
   if ($scope.operation === '+') {
     $scope.add($scope.x,$scope.y);
     $scope.display = $scope.result;
-    }else if ($scope.operation === '-'){
+    }
+    else if ($scope.operation === '-'){
       $scope.subtract($scope.x,$scope.y);
       $scope.display = $scope.result;
-  }
+      }
+    else if ($scope.operation === '*') {
+        $scope.multiply($scope.x,$scope.y);
+        $scope.display = $scope.result;
+      }
+    else if ($scope.operation === '&#47') {
+        $scope.divide($scope.x,$scope.y);
+        $scope.display = $scope.result;
+      }
+    else if ($scope.operation === '&#x207b;&sup1') {
+        $scope.sin($scope.x);
+        $scope.display = $scope.result;
+      }
+    // else if ($scope.operation === '&radic') {
+    //     $scope.squared($scope.x);
+    //     $scope.display = $scope.result;
+    //   }
 }
 
 
